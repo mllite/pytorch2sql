d8734338e8ad76e054d006201e039fcf598ac5cf
Antoine Carme <antoine.carme@laposte.net>
2021-07-25 09:55:16 +0200

    Use a skorch model in this script

--- test_client.py
+++ test_client.py
@@ -3,15 +3,48 @@ import pickle, json, requests, base64
 
 from sklearn import datasets
 
+
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+import skorch
+from skorch import NeuralNetClassifier
+
+torch.manual_seed(1960)
+
+torch.set_default_tensor_type('torch.DoubleTensor')
+
+
+
+def create_model():
+    hidden_units = 15
+    num_classes = 3
+    num_inputs = 4
+    model = nn.Sequential(
+        nn.Linear(num_inputs, hidden_units),
+        nn.ReLU(),
+        nn.Dropout(),
+        nn.Linear(hidden_units , num_classes),
+        nn.Softmax())
+
+    return model
+
 iris = datasets.load_iris()
 X = iris.data  
 Y = iris.target
 # print(iris.DESCR)
 
-from sklearn.neural_network import MLPClassifier
-clf = MLPClassifier()
-clf.fit(X, Y)
+clf = skorch.NeuralNetClassifier(
+    create_model(),
+    optimizer=torch.optim.Adam,
+    max_epochs=10,
+)
 
+
+print(X.shape , Y.shape)
+clf.fit(X, Y)
 
 def test_ws_sql_gen(pickle_data):
     WS_URL="https://sklearn2sql.herokuapp.com/model"
